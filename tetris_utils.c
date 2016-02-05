697aa62ccc6ad9c6b95a5ed6c6c1299f7db7fa50
ComtriS <kachikosbodkin@gmail.com>
2016-02-05 15:08:38 -0800

    Collisions with side wall and pieces is working

--- tetris_utils.c
+++ tetris_utils.c
@@ -9,6 +9,10 @@
 #define SUCCESS         0
 #define ERR_GENERIC    -1
 #define ERR_PARAMS     -2
+#define ERR_OFF_TOP    -3
+#define ERR_OFF_BOTTOM -4
+#define ERR_OFF_LEFT   -5
+#define ERR_OFF_RIGHT  -6
 
 piece_t piece_list[TOTAL_PIECES] = {0};
 
@@ -70,26 +74,35 @@ void console_clear(void)
 	write(STDOUT_FILENO, " \033[1;1H\033[2J", 12);
 }
 
-void input_handle(keycode_t input, int* row, int* col)
+void input_handle(board_t* b, keycode_t input, piece_t piece, int* row, int* col)
 {
+	int new_row = *row;
+	int new_col = *col;
+	
 	switch (input) {
 		case KEY_LEFT:
-			(*col)--;
+			new_col--;
 			break;
 		case KEY_RIGHT:
-			(*col)++;
+			new_col++;
 			break;
 		case KEY_UP:
 			// do nothing
 			break;
 		case KEY_DOWN:
-			(*row)++;
+			new_row++;
 			break;
 		default:
 			// do nothing
 			// printf("UNKNOWN\n");
 			break;
 	}
+	
+	if (collision_check(b, piece, new_row, new_col))
+		return;
+	
+	*row = new_row;
+	*col = new_col;
 }
 
 keycode_t get_input(void)
@@ -106,22 +119,33 @@ keycode_t get_input(void)
 	return (keycode_t)getchar();
 }
 
+bool collision_check_bottom(board_t* b, int row, int col)
+{
+	int val = board_get(b, row, col);
+	return (val || val == ERR_OFF_BOTTOM);
+}
+
+bool collision_check(board_t* b, piece_t piece, int row, int col)
+{
+	TRACE("%s(%d, %d)\n", __func__, row, col);
+	
+	for (int y = 0; y < MAX_PIECE_HEIGHT; y++)
+		for (int x = 0; x < MAX_PIECE_WIDTH; x++)
+			if (piece.cells[y][x])
+				if (board_get(b, row + y, col + x) != 0)
+					return true;
+	return false;
+}
+
 bool anchor_check(board_t* b, piece_t piece, int row, int col)
 {
 	TRACE("%s(%d, %d)\n", __func__, row, col);
 	
-	// check anchoring with other pieces
-	for (int y = MAX_PIECE_HEIGHT-1; y > 0; y--) {
-		for (int x = 0; x < MAX_PIECE_WIDTH; x++) {
-			if (piece.cells[y][x]) {
-				int val = board_get(b, y + row + 1, x + col);
-				if (val == ERR_PARAMS || val == true) {
-					TRACE("found a collision at (%d, %d)\n", y + row + 1, x + col);
+	for (int y = MAX_PIECE_HEIGHT-1; y > 0; y--)
+		for (int x = 0; x < MAX_PIECE_WIDTH; x++)
+			if (piece.cells[y][x])
+				if (collision_check_bottom(b, y + row + 1, x + col))
 					return true;
-				}
-			}
-		}
-	}
 	
 	return false;
 }
@@ -153,10 +177,14 @@ void board_set(board_t* b, int row, int col, int val)
 int board_get(board_t* b, int row, int col)
 {
 	TRACE("%s(%p, %d, %d)\n", __func__, (void*)b, row, col);
-	if (row < 0 || row >= b->height)
-		return ERR_PARAMS;
-	if (col < 0 || col >= b->width)
-		return ERR_PARAMS;
+	if (row < 0)
+		return ERR_OFF_TOP;
+	if (row >= b->height)
+		return ERR_OFF_BOTTOM;
+	if (col < 0)
+		return ERR_OFF_LEFT;
+	if (col >= b->width)
+		return ERR_OFF_RIGHT;
 	return b->cells[row][col];
 }
 
